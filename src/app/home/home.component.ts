7c77e7fa63756c16879793064397e0abf0554f61
amjadkn <mail2amjad87@gmail.com>
2020-12-29 22:47:22 +0500

    Image Replaced with CSS in progress and complete pages, and john D coloumn is getting updated from server in home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,18 +1,21 @@
-import { NavBarService } from './../services/nav-bar.service';
-import { ChartsService } from './../services/charts.service';
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import * as d3 from 'd3';
 import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+
+import { WebsocketService } from './../services/websocket.service';
+import { NavBarService } from './../services/nav-bar.service';
+import { ChartsService } from './../services/charts.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit, AfterViewInit {
+export class HomeComponent implements OnInit, AfterViewInit, OnDestroy {
 
   @ViewChild('available', {read: ElementRef}) elRef;
-
+  private completed = false;
   private data = [
     {"complete": "100"},
     {"complete": "0"},
@@ -30,6 +33,12 @@ export class HomeComponent implements OnInit, AfterViewInit {
     {"complete": "0"},
   ];;
 
+  private _current: any;
+  private arc: any;
+  private pie: any;
+  private path: any;
+
+  private subscription: Subscription;
   private svg;
   private svg2;
   private svg3;
@@ -43,7 +52,12 @@ export class HomeComponent implements OnInit, AfterViewInit {
   private completeColors;
   private unavailableColors;
 
-  constructor(private chartsService: ChartsService, private router: Router, private navService: NavBarService) { }
+  constructor(
+    private chartsService: ChartsService,
+    private router: Router,
+    private navService: NavBarService,
+    private wsService: WebsocketService
+    ) { }
 
   ngOnInit(): void {
     this.navService.show();
@@ -53,6 +67,22 @@ export class HomeComponent implements OnInit, AfterViewInit {
     this.createSvg();
     this.createColors();
     this.drawChart();
+
+    this.wsService.getChartsData("ready", 0, "john");
+    this.subscription = this.wsService.getServerMessageSubject()
+    .subscribe(message => {
+
+      if(message.percent_complete) {
+        if(message.percent_complete === 100) {
+          this.completed = true;
+        }
+      }
+      const updatedData = [
+        {"complete": message.percent_complete},
+        {"complete": 100 - message.percent_complete},
+      ];
+      this.change(updatedData);
+    });
   }
 
   private createSvg(): void {
@@ -125,12 +155,12 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   private drawChart(): void {
     // Compute the position of each group on the pie:
-    const pie = d3.pie<any>().value((d: any) => Number(d.complete)).sort(null);
+    this.pie = d3.pie<any>().value((d: any) => Number(d.complete)).sort(null);
 
     // Build the pie chart
     this.svg
     .selectAll('pieces')
-    .data(pie(this.data))
+    .data(this.pie(this.data))
     .enter()
     .append('path')
     .attr('d', d3.arc()
@@ -142,23 +172,29 @@ export class HomeComponent implements OnInit, AfterViewInit {
     .style("stroke-width", "1px");
 
     // Build the pie chart
-    this.svg2
+    this.path = this.svg2
     .selectAll('pieces')
-    .data(pie(this.johnData))
+    .data(this.pie(this.johnData))
     .enter()
-    .append('path')
-    .attr('d', d3.arc()
-      .innerRadius(90)
-      .outerRadius(this.radius)
-    )
-    .attr('fill', (d, i) => (this.johnColors(i)))
-    .attr("stroke", "#000")
-    .style("stroke-width", "1px");
+    .append('path');
+
+    this.arc = d3.arc()
+    .innerRadius(90)
+    .outerRadius(this.radius);
+
+    this.path.transition()
+      .duration(500)
+      .attr("fill", (d, i) => this.johnColors(i))
+      .attr("stroke", "#121926")
+      .style("stroke-width", "1px")
+      .attr('d', this.arc)
+      .each(function(d) { this._current = d; }); // store the initial angles
+
 
     // Build the pie chart
     this.svg3
     .selectAll('pieces')
-    .data(pie(this.completeData))
+    .data(this.pie(this.completeData))
     .enter()
     .append('path')
     .attr('d', d3.arc()
@@ -172,7 +208,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     // Build the pie chart
     this.svg4
     .selectAll('pieces')
-    .data(pie(this.unavailableData))
+    .data(this.pie(this.unavailableData))
     .enter()
     .append('path')
     .attr('d', d3.arc()
@@ -194,5 +230,22 @@ export class HomeComponent implements OnInit, AfterViewInit {
 
   onCompleteButtonClicked() {
     this.router.navigate(['/complete']);
+  }
+
+  change(data) {
+    this.path.data(this.pie(data));
+    this.path.transition().duration(750).attrTween("d", this.arcTween); // redraw the arcs
+  }
+
+  arcTween = (datum, index) => {
+    const interpolation = d3.interpolate(this._current, datum);
+    // this._current[index] = interpolation(0);
+    return (t) => {
+        return this.arc(interpolation(t));
+    }
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
